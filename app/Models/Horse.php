c80ccab9f752d1dcae4e214a30c716798eaa88b2
Muhammad Ali Mughal <muhammadalimughal53@gmail.com>
2023-11-30 00:13:31 +0500

    ND-797 - File importer. * Completed

--- app/Models/Horse.php
+++ app/Models/Horse.php
@@ -8,4 +8,39 @@ use Illuminate\Database\Eloquent\Model;
 class Horse extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'race_id',
+        'track_name',
+        'date',
+        'previous_race',
+        'name',
+        'weight_carried',
+        'age',
+        'gender',
+        'equipment_id',
+        'jockey',
+        'win_odds',
+        'claiming_price',
+        'finish_position',
+        'trainer',
+        'owner',
+        'data',
+    ];
+
+    protected $casts = [
+        'data' => 'json',
+    ];
+
+    // Override the set accessor for the 'data' attribute
+    public function setDataAttribute($value)
+    {
+        $this->attributes['data'] = json_encode($value);
+    }
+
+    // Override the get accessor for the 'data' attribute
+    public function getDataAttribute($value)
+    {
+        return json_decode($value, true);
+    }
+
 }
--- database/migrations/2023_11_24_102814_create_horses_table.php
+++ database/migrations/2023_11_24_102814_create_horses_table.php
@@ -16,22 +16,23 @@ return new class extends Migration
             $table->unsignedBigInteger('race_id');
             $table->string('track_name');
             $table->timestamp('date')->comment('date of race');
+            $table->string('previous_race')->nullable()->comment('date of previous race');
             $table->string('name')->comment('horse name');
             $table->integer('weight_carried')->comment('weight carried by horse');
             $table->integer('age')->comment('horse age');
-            $table->string('gender')->comment('horse gender');
-            $table->unsignedBigInteger('equipment_id');
-            $table->string('jockey')->comment('Jockey name');
+            $table->string('gender')->nullable()->comment('horse gender');
+            $table->unsignedBigInteger('equipment_id')->nullable();
+            $table->string('jockey')->nullable()->comment('Jockey name');
             $table->integer('win_odds')->comment('Win Odds (Net win per $1 bet - excludes the $1 bet, so 1.4 means 2.4 is returned)');
             $table->integer('claiming_price');
             $table->integer('finish_position')->comment('position at which horse finished the race');
-            $table->string('trainer')->comment('Trainer name');
-            $table->string('owner')->comment('Owner name');
+            $table->string('trainer')->nullable()->comment('Trainer name');
+            $table->string('owner')->nullable()->comment('Owner name');
             $table->json('data')->comment('save complete row as json');
             $table->timestamps();
             $table->softDeletes();
 
-            $table->foreign('race_id')->references('id')->on('races');
+            $table->foreign('race_id')->references('id')->on('races')->onDelete('cascade');
         });
     }
 
